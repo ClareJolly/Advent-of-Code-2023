ce158174d0156bbbaf28d39f6c98233b8c257d7f
Clare Jolly <clare.jolly@vodafone.com>
2023-12-02 16:27:27 +0000

    day 2 part 2 complete (pre-rafactoring)

--- src/days/02/part2/index.ts
+++ src/days/02/part2/index.ts
@@ -1,3 +1,39 @@
-const part2 = (inputData: string[]): number => {}
+import parseGames from '../helpers/parseGames'
+import { AvailableColors, Game } from '../types'
+
+export type SummaryData = { [key in AvailableColors]: number[] }
+export type MinValuesData = { [key in AvailableColors]: number }
+export interface Summary extends Game {
+  summary: SummaryData
+  minValues: MinValuesData
+  power: number
+}
+
+const part2 = (inputData: string[]): number => {
+  const games = parseGames(inputData)
+  const summary: Summary[] = games.map(game => {
+    const data: SummaryData = {} as SummaryData
+    game.subsets.forEach(set => {
+      Object.entries(set).forEach(([color, num]) => {
+        if (!data[color as AvailableColors]) data[color as AvailableColors] = []
+        data[color as AvailableColors].push(num)
+      })
+    })
+    const minValues = Object.entries(data).reduce((acc, [col, num]) => {
+      acc[col as AvailableColors] = Math.max(...num)
+      return acc
+    }, {} as MinValuesData)
+
+    const power = Object.values(minValues).reduce((acc, num) => {
+      return acc * num
+    }, 1)
+
+    return { ...game, summary: data, minValues, power }
+  })
+
+  return summary.reduce((acc, game) => {
+    return acc + game.power
+  }, 0)
+}
 
 export default part2
