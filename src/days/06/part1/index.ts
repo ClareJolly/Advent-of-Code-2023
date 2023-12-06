397b555037ab86b3d001be158254dd7be3276934
Clare Jolly <clare.jolly@vodafone.com>
2023-12-06 13:21:08 +0000

    day 6 part 1 done - no refactoring yet - and part 2 set up

--- src/days/06/part1/index.ts
+++ src/days/06/part1/index.ts
@@ -1,3 +1,30 @@
-const part1 = (inputData: string[]): number => {}
+const part1 = (inputData: string[]): number => {
+  const time = inputData[0].match(/\d+/g)!.map(Number)
+  const distance = inputData[1].match(/\d+/g)!.map(Number)
+
+  const data = time.map((t, i) => ({ time: t, distance: distance[i] }))
+
+  const summary = data.map(d => {
+    const holdOptions = []
+    for (let i = 1; i < d.time; i++) {
+      const hold = i
+      const remainingTime = d.time - i
+      const distanceTravelled = i * remainingTime
+      holdOptions.push({ hold, remainingTime, distanceTravelled })
+    }
+
+    return {
+      ...d,
+      holdOptions,
+    }
+  })
+
+  // console.dir(summary, { depth: null })
+  const waysToWin = summary.map(s => {
+    return s.holdOptions.filter(h => h.distanceTravelled > s.distance).length
+  })
+
+  return waysToWin.reduce((acc, item) => acc * item, 1)
+}
 
 export default part1
